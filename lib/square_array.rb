312efbdbc03e8ce2693ca3319c7477f1f1ae50de
Daniel Sasse <dsasse07@gmail.com>
2020-11-11 20:27:03 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def square_array(array)
-  # your code here
+  # your code here 
+  index = 0
+  while index < array.length do 
+    array[index] = array[index]**2
+    index +=1 
+  end
+  array
 end
